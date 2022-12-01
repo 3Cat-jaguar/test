54b184a099cceb81cf1644eeadc6ab3537200668
YuriLee <yuri.lee@sisoul.co.kr>
2022-12-01 17:24:58 +0900

    set viewBinding

--- app/src/main/java/kr/co/cat_diversity/ylee/breath_guide/MainActivity.kt
+++ app/src/main/java/kr/co/cat_diversity/ylee/breath_guide/MainActivity.kt
@@ -2,10 +2,14 @@ package kr.co.cat_diversity.ylee.breath_guide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.cat_diversity.ylee.breath_guide.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
